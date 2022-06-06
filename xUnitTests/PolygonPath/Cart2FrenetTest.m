a7faac0c03aa0ab6ba0fb855e007a90418fef063
Georg <84226458+gnestlinger@users.noreply.github.com>
2022-06-06 15:37:55 +0200

    Set max. angular error via input argument in methods cart2frenet() and pointProjection().

--- xUnitTests/PolygonPath/Cart2FrenetTest.m
+++ xUnitTests/PolygonPath/Cart2FrenetTest.m
@@ -4,7 +4,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
         
         function testUniqueSolutions(testCase)
             obj = PolygonPath.xy2Path(1:4, [2 2 2 2]);
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [2 0], false);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([2 0], [], false);
             testCase.verifyEqual(sd, [1 2]);
             testCase.verifyEqual(Q, [2 2]);
             testCase.verifyEqual(idx, 2);
@@ -16,7 +16,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
             obj = PolygonPath.xy2Path(1:4, [2 2 2 2]);
             
             % Initial point solution
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [1 0], false);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([1 0], [], false);
             testCase.verifyEqual(sd, [0 2]);
             testCase.verifyEqual(Q, [1 2]);
             testCase.verifyEqual(idx, 1);
@@ -24,7 +24,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
             testCase.verifyEqual(dphi, 0);
             
             % End point solution
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [4 0], false);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([4 0], [], false);
             testCase.verifyEqual(sd, [3 2]);
             testCase.verifyEqual(Q, [4 2]);
             testCase.verifyEqual(idx, 3);
@@ -34,7 +34,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
         
         function testMultipleSolutions(testCase)
             obj = PolygonPath.circle(3, [-pi pi]/2, 10);
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [0 0], false);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([0 0], [], false);
             
             N = 9; % One solution per path segment
             testCase.verifySize(sd, [N 2]);
@@ -50,7 +50,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
             [P0,P1] = obj.termPoints();
             
             % Fallback initial point
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [-1 -1], false);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([-1 -1], [], false);
             testCase.verifyEqual(sd, [0 sqrt(2)]);
             testCase.verifyEqual(Q, P0');
             testCase.verifyEqual(idx, 1);
@@ -58,7 +58,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
             testCase.verifyEqual(dphi, pi/4);
             
             % Fallback end point
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [2 1], false);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([2 1], [], false);
             testCase.verifyEqual(sd, [1 -sqrt(2)]);
             testCase.verifyEqual(Q, P1');
             testCase.verifyEqual(idx, 1);
@@ -70,7 +70,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
         function testCircuitPath(testCase)
             obj = PolygonPath.circle(3, [0 2*pi], 15);
             assert(obj.IsCircuit)
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [0 0], 0);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([0 0], [], 0);
             
             % One solution per path segment, at the center of each segment
             N = numel(obj) - 1;
@@ -85,7 +85,7 @@ classdef Cart2FrenetTest < matlab.unittest.TestCase
         function testSignD(testCase)
             obj = PolygonPath.circle(1, [0 2*pi], 9);
             assert(obj.IsCircuit)
-            [sd,Q,idx,tau,dphi] = cart2frenet(obj, [1 0.5], false);
+            [sd,Q,idx,tau,dphi] = obj.cart2frenet([1 0.5], false);
             
             testCase.verifyTrue(sd(1,2) > 0);
             testCase.verifyTrue(sd(2,2) < 0);
