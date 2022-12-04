4b65465cb7e991d08966e22bb0ef4b52b5e53ab5
Georg <84226458+gnestlinger@users.noreply.github.com>
2022-12-04 12:11:48 +0100

    Test cases for SplinePath added; test file moved.

--- xUnitTests/ShiftTest.m
+++ xUnitTests/ShiftTest.m
@@ -2,40 +2,44 @@ classdef ShiftTest < matlab.unittest.TestCase
     
     properties (TestParameter)
         obj = struct(...
-            'emptyPath', PolygonPath([], [], [], []), ...
-            'oneElmPath', PolygonPath(1, 2, pi/4, 0), ...
-            'nonEmptyPath', PolygonPath.xy2Path(10:-1:0, zeros(1,11)));
+            'PolygonPathEmpty', PolygonPath([], [], [], []), ...
+            'PolygonPathOneElm', PolygonPath(1, 2, pi/4, 0), ...
+            'PolygonPathNonEmpty', PolygonPath.xy2Path(10:-1:0, zeros(1,11)), ...
+            'SplinePathEmpty', SplinePath(), ...
+            'SplinePathOneElm', SplinePath.pp2Path(mkpp([-1 2], [0 1 -1; 1 0 2], 2)));
     end
     
     
     methods (Test)
         
         function testShift(testCase, obj)
-			
+            
             [P0,P1] = obj.termPoints();
             
             P = [1; 1];
-			objs = obj.shift(P);
-			[Ps0,Ps1] = objs.termPoints();
-            if ~isempty(obj)
+            objs = obj.shift(P);
+            [Ps0,Ps1] = objs.termPoints();
+            if isempty(obj)
+                testCase.verifyTrue(all(isnan([P0; P1; Ps0; Ps1])))
+            else
                 testCase.verifyEqual(Ps0, P0+P);
                 testCase.verifyEqual(Ps1, P1+P);
             end
             
-		end%fcn
+        end%fcn
         
         function testDefaultArg(testCase, obj)
-			
+            
             [P0,P1] = obj.termPoints();
             
-			objs = obj.shift();
-			[Ps0,Ps1] = objs.termPoints();
+            objs = obj.shift();
+            [Ps0,Ps1] = objs.termPoints();
             if ~isempty(obj)
                 testCase.verifyEqual(Ps0, [0;0]);
                 testCase.verifyEqual(Ps1, P1-P0);
             end
             
-		end%fcn
+        end%fcn
         
     end
     
