0c7d4f7599d32c36509f5f4b12101cd3177e58b4
Georg Nestlinger <84226458+gnestlinger@users.noreply.github.com>
2024-04-19 15:25:30 +0200

    Update ramerDouglasPeucker.m
    Handle case of matching terminal points.

--- private/ramerDouglasPeucker.m
+++ private/ramerDouglasPeucker.m
@@ -24,9 +24,8 @@ function dprec(idx0, idx1)
 	d = perpendicularDistance(x(idx0:idx1), y(idx0:idx1));
 	[val_max,idx_max] = max(abs(d));
 	if val_max > eps
-		% Split waypoints at IDX_SPLIT and call recursion with
-		% those two resulting segments until we end in the else
-		% statement.
+		% Split waypoints at IDX_SPLIT and call recursion with those two
+		% resulting segments until we end in the else statement.
 		idx_split = idx_max + idx0 - 1;
 		dprec(idx0, idx_split);
 		dprec(idx_split, idx1);
@@ -52,6 +51,11 @@ x1 = x(1);
 y1 = y(1);
 dx = x(end) - x1;
 dy = y(end) - y1;
-d = abs(dx*(y1-y) - dy*(x1-x)) / sqrt(dx^2 + dy^2);
+h = hypot(dx, dy);
+if h ~= 0
+    d = abs(dx*(y-y1) - dy*(x-x1))/h;
+else
+    d = hypot(x-x1, y-y1);
+end
 
 end%fcn
