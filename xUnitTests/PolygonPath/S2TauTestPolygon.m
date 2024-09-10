ffe3bbf2caaf4b82740a0a2dabb6dbd963f5d60f
Georg <84226458+gnestlinger@users.noreply.github.com>
2024-09-10 19:38:09 +0200

    Test cosmetics.

--- xUnitTests/PolygonPath/S2TauTestPolygon.m
+++ xUnitTests/PolygonPath/S2TauTestPolygon.m
@@ -1,10 +1,6 @@
 classdef S2TauTestPolygon < matlab.unittest.TestCase
     
     properties (TestParameter)
-        Path = struct(...
-            'emptyPath', PolygonPath(), ...
-            'oneElmPath', PolygonPath(1, 2, pi/4, 0), ...
-            'nonEmptyPath', PolygonPath.xy2Path(0:10, zeros(1,11)))
         s = struct(...
             'emptyTau', [], ...
             'oneElmTau', 1.1, ...
@@ -16,12 +12,6 @@ classdef S2TauTestPolygon < matlab.unittest.TestCase
     
     methods (Test)
         function testReturnValuesZeroLengthPath(testCase, s)
-            % Empty path
-            obj = PolygonPath();
-            [tau,idx] = obj.s2tau(s);
-            verifyEqual(testCase, tau, nan(size(s)));
-            verifyEqual(testCase, idx, zeros(size(s), 'uint32'));
-            
             % Non-empty but zero length path
             obj = PolygonPath(1,2,3,4);
             [tau,idx] = obj.s2tau(s);
@@ -29,14 +19,13 @@ classdef S2TauTestPolygon < matlab.unittest.TestCase
             verifyEqual(testCase, idx, zeros(size(s), 'uint32'));
         end%fcn
         
-        function testReturnValues(testCase)
+        function testReturnValuesNonEmptyPath(testCase)
             obj = PolygonPath.xy2Path([-3 1 8 34], [3 1 0 6]);
             s = [-1 -0.2 0 0.1 100 2 4 5 9 20]; %#ok<*PROP>
             [tau,idx] = obj.s2tau(s);
             
             [tau0,tau1] = obj.domain();
-            cl = [0 obj.cumlengths()'];
-            tauSet = interp1(cl, tau0:tau1, s, 'linear','extrap');
+            tauSet = interp1([0 obj.cumlengths()'], tau0:tau1, s, 'linear','extrap');
             
             verifyEqual(testCase, tau, tauSet, 'AbsTol',1e-15)
             verifyEqual(testCase, idx, uint32([1 1 1 1 3 1 1 2 2 3]));
